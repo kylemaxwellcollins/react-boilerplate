6669202e9fd27d0e2b20ab64315aa77e52ee68fd
kylemaxwellcollins <kylemaxwellcollins@gmail.com>
2018-12-29 14:38:07 -0500

    initial commit :rocket:

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "normalize.css/normalize.css"
+import "./styles/styles.scss"
+
+const template = (<p>Hello World</p>)
+
+ReactDOM.render(template, document.getElementById("app"));
