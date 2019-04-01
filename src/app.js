f294cd1d429b961c62e6edd43a069542f7408144
Kyle Collins <kyle.collins@Kyle-Collins-MBP15R.local>
2019-04-01 11:35:15 -0400

    Added ESlint config with Prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "normalize.css/normalize.css"
-import "./styles/styles.scss"
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-const template = (<p>Hello World</p>)
+const template = <p>Hello World</p>;
 
-ReactDOM.render(template, document.getElementById("app"));
+ReactDOM.render(template, document.getElementById('app'));
